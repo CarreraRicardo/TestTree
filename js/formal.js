e71da093db0096ddd64de3a1edb6ae7f882e3745
Ricardo Carrera <ricardo@internetpowerhotel.com>
2019-12-05 13:58:21 -0700

    Se hizo un rollback a estructura del proyecto

---
+++ js/formal.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param {string} greeting 
+ */
+
+function GreetingFormal(greeting) {
+    return greeting;
+}
+
+/**
+ * 
+ * @param {string} farewell 
+ */
+
+function FarewellFormal(farewell) {
+    return farewell;
+}
---
+++ js/informal.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param {string} greeting 
+ */
+
+function GreetingInformal(greeting) {
+    return greeting;
+}
+
+/**
+ * 
+ * @param {string} farewell 
+ */
+
+function FarewellInformal(farewell) {
+    return farewell;
+}
