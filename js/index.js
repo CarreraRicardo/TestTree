6dd8b9078d6a6d130e3a2dc7e3f6190bad456150
Ricardo Carrera <ricardo@internetpowerhotel.com>
2019-12-05 13:38:13 -0700

    Generic Responses

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param {string} greeting 
+ */
+
+function Greeting(greeting) {
+    return greeting;
+}
+
+/**
+ * 
+ * @param {string} farewell 
+ */
+
+function Farewell(farewell) {
+    return farewell;
+}
